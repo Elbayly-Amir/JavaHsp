e9733a6a79c853ab5fd5623982f31a8a76fa5c64
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-01-16 13:36:07 +0100

    change scene

--- src/main/java/com/example/javahsp/HelloApplication.java
+++ src/main/java/com/example/javahsp/HelloApplication.java
@@ -10,17 +10,51 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    private static Stage stage;
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage firstStage) throws IOException {
+        stage=firstStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Accueil.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("HSP");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+
+
+    public static void changeScene(String fichierFxml)  {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(fichierFxml+".fxml"));
+        Scene scene = null;
+        try {
+
+            scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Connexion");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void changeScene(String fichierFxml, Object controller)  {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(fichierFxml+".fxml"));
+        Scene scene = null;
+        try {
+
+            fxmlLoader.setController(controller);
+            scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Inscription");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 

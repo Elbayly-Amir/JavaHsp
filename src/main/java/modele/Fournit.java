e4142c35a1cb0d6c20f26837fc050876910a91f4
amir <a.elbayly@lprs.fr>
2023-01-22 21:29:56 +0100

    class fournit

---
+++ src/main/java/modele/Fournit.java
@@ -0,0 +1,25 @@
+package modele;
+
+public class Fournit {
+
+
+    private int ref_produit;
+    private int ref_fournisseur;
+
+
+    public int getRef_produit() {
+        return ref_produit;
+    }
+
+    public void setRef_produit(int ref_produit) {
+        this.ref_produit = ref_produit;
+    }
+
+    public int getRef_fournisseur() {
+        return ref_fournisseur;
+    }
+
+    public void setRef_fournisseur(int ref_fournisseur) {
+        this.ref_fournisseur = ref_fournisseur;
+    }
+}

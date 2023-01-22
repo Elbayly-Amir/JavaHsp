4dffaa8d6cf0b1c6ca8b37411b911c0e46836a29
amir <a.elbayly@lprs.fr>
2023-01-22 21:27:43 +0100

    class chambre

---
+++ src/main/java/modele/Chambre.java
@@ -0,0 +1,49 @@
+package modele;
+
+public class Chambre {
+
+    private int id_chambre;
+    private String Occupation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int getId_chambre() {
+        return id_chambre;
+    }
+
+    public void setId_chambre(int id_chambre) {
+        this.id_chambre = id_chambre;
+    }
+
+    public String getOccupation() {
+        return Occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        Occupation = occupation;
+    }
+}

9e89ea5523f097c5d2e60d53ba563decebd4485e
amir <a.elbayly@lprs.fr>
2023-01-22 21:31:15 +0100

    class Regit

---
+++ src/main/java/modele/Regit.java
@@ -0,0 +1,24 @@
+package modele;
+
+public class Regit {
+
+    private int ref_medecin;
+    private int ref_fichesortit;
+
+
+    public int getRef_medecin() {
+        return ref_medecin;
+    }
+
+    public void setRef_medecin(int ref_medecin) {
+        this.ref_medecin = ref_medecin;
+    }
+
+    public int getRef_fichesortit() {
+        return ref_fichesortit;
+    }
+
+    public void setRef_fichesortit(int ref_fichesortit) {
+        this.ref_fichesortit = ref_fichesortit;
+    }
+}

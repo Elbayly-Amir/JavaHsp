50959d56bb13f35f2f1b0b882be48d788083de04
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-01-23 13:24:30 +0100

    admin

---
+++ src/main/java/com/example/javahsp/EspaceAdmin.java
@@ -0,0 +1,28 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EspaceAdmin {
+
+
+    @FXML
+    void crudMedecin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crudSecretaire(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crudgestionnaire(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retourAdmin(ActionEvent event) {
+
+    }
+}

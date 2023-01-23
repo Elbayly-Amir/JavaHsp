271e99cdda2c931f00f4c9d49ee269e97542260d
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-01-23 13:24:12 +0100

    gestionnaire

---
+++ src/main/java/com/example/javahsp/CrudGestionnaire.java
@@ -0,0 +1,22 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class CrudGestionnaire {
+
+    @FXML
+    void ajoutGestionnaire(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retourCrudgestionnaire(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visuGestionnaire(ActionEvent event) {
+
+    }
+}

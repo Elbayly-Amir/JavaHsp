91b3de346a444aeed40318a3d0a6c041c51ec088
Rayan Lazar <r.lazar@lprs.fr>
2023-01-23 10:06:42 +0100

    Ajout de la page de Connexion Gestionnaire de stock

--- src/main/java/com/example/javahsp/ConnexionGestionnaire.java
+++ src/main/java/com/example/javahsp/ConnexionGestionnaire.java
@@ -1,4 +1,26 @@
 package com.example.javahsp;
 
 public class ConnexionGestionnaire {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField emailGest;
+
+    @FXML
+    private PasswordField mdpGest;
+
+    @FXML
+    void connexionGest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retourGest(ActionEvent event) {
+
+    }
+
 }

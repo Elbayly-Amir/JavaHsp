d6923610e88944b5da1f2bd30b45b0d5ad743b45
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-01-23 13:33:45 +0100

    secretaire

---
+++ src/main/java/com/example/javahsp/AjoutSecretaire.java
@@ -0,0 +1,25 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AjoutSecretaire {
+
+    @FXML
+    private TextField emailSecretaire;
+
+    @FXML
+    private TextField mdpSecretaire;
+
+    @FXML
+    private TextField nomSecretaire;
+
+    @FXML
+    private TextField prenomSecretaire;
+
+    @FXML
+    void ajoutSecretaire(ActionEvent event) {
+
+    }
+}

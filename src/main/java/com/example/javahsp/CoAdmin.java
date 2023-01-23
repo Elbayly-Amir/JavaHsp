6be4c66f4d00c4923918b334b7b9c663e72fb92f
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-01-23 12:27:20 +0100

    modèle admin + connexion admin

---
+++ src/main/java/com/example/javahsp/CoAdmin.java
@@ -0,0 +1,47 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import modele.Admin;
+
+import java.sql.SQLException;
+
+public class CoAdmin {
+
+
+
+
+    @FXML
+    private TextField emailAdmin;
+
+    @FXML
+    private TextField mdpAdmin;
+
+    public CoAdmin(String text, String text1) {
+    }
+
+    public CoAdmin() {
+
+    }
+
+    @FXML
+    void connexionAdmin(ActionEvent event) throws SQLException {
+        Admin adm = new Admin(emailAdmin.getText(), mdpAdmin.getText());
+        Admin a = adm.connexion();
+        if(a == null){
+            System.out.println(" id introuvable");
+        }else{
+            System.out.println("user connecté!");
+        }
+        System.out.println("connexion");
+        HelloApplication.changeScene("espaceAdmin");
+
+    }
+
+    @FXML
+    void retourAdmin(ActionEvent event) {
+        CoAdmin cad = new CoAdmin();
+        HelloApplication.changeScene("Accueil");
+    }
+}
---
+++ src/main/java/modele/Admin.java
@@ -0,0 +1,81 @@
+package modele;
+
+import BDD.BDD;
+import javafx.scene.control.TextField;
+
+import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class Admin {
+
+
+
+    private String email;
+    private String mdp;
+    private int id_admin;
+
+    boolean estConnecte = false;
+
+    public Admin(String email, String mdp) {
+        this.email = email;
+        this.mdp = mdp;
+    }
+
+    public Admin (Admin a){
+    }
+
+    public Admin connexion() throws SQLException {
+        Admin a = null;
+        while(!estConnecte){
+
+        BDD mabdd = new BDD();
+
+
+            PreparedStatement maRequete = mabdd.getBDD().prepareStatement("Select * from admin where email=? and mdp=?");
+            maRequete.setString(1,email);
+            maRequete.setString(2,mdp);
+            ResultSet mesResultats = maRequete.executeQuery();
+
+            TextField email = null;
+            TextField mdp = null;
+            if (mesResultats.next()){
+                a = new Admin(a);
+                a.setId_admin(1);
+                a.setEmail(mesResultats.getString("email"));
+
+                estConnecte = true;
+            }else{
+                System.out.println("erreur");
+            }
+        }
+        return a;
+    }
+
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public int getId_admin() {
+        return id_admin;
+    }
+
+    public void setId_admin(int id_admin) {
+        this.id_admin = id_admin;
+    }
+}

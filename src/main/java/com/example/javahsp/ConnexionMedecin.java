9a7610e7225d689d69a308bda9bce7060f240a43
Rayan Lazar <r.lazar@lprs.fr>
2023-01-27 13:36:40 +0100

    Modification Pour la Connexion Medecin

--- src/main/java/com/example/javahsp/ConnexionMedecin.java
+++ src/main/java/com/example/javahsp/ConnexionMedecin.java
@@ -3,6 +3,10 @@ package com.example.javahsp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import modele.Admin;
+import modele.Medecin;
+
+import java.sql.SQLException;
 
 import static com.example.javahsp.HelloApplication.changeScene;
 
@@ -16,9 +20,19 @@ public class ConnexionMedecin {
     private TextField mdpMedecin;
 
     @FXML
-    void connexionMedecin(ActionEvent event) {
+    void connexionMedecin(ActionEvent event) throws SQLException {
         ConnexionMedecin cm = new ConnexionMedecin();
-        changeScene("EspaceMedecin");
+        //changeScene("EspaceMedecin");
+
+        Medecin mdc = new Medecin(emailMedecin.getText(), mdpMedecin.getText());
+        Medecin m = mdc.connexion();
+        if(m == null){
+            System.out.println("compte introuvable");
+        }else{
+            System.out.println("user connecté!");
+        }
+        System.out.println("connexion");
+        HelloApplication.changeScene("espaceMedecin");
 
     }
 

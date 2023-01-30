ed79e8386006dcdf8e9e104cefffe0aedeb09b6e
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-01-30 11:56:19 +0100

    espace admin

--- src/main/java/com/example/javahsp/EspaceAdmin.java
+++ src/main/java/com/example/javahsp/EspaceAdmin.java
@@ -2,27 +2,47 @@ package com.example.javahsp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
 
 public class EspaceAdmin {
 
 
     @FXML
-    void crudMedecin(ActionEvent event) {
-            HelloApplication.changeScene("crudMedecin");
+    private Tab gestionnaire;
+
+    @FXML
+    private Tab medecin;
+
+    @FXML
+    private Tab secretaire;
+
+    @FXML
+    private TableView<?> tableViewGestionnaire;
+
+    @FXML
+    private TableView<?> tableViewMedecin;
+
+    @FXML
+    private TableView<?> tableViewSecretaire;
+
+    @FXML
+    void AjoutMedecin(ActionEvent event) {
+        HelloApplication.changeScene("ajoutMeedecin");
     }
 
     @FXML
-    void crudSecretaire(ActionEvent event) {
-            HelloApplication.changeScene("crudSecretaire");
+    void AjoutSecretaire(ActionEvent event) {
+        HelloApplication.changeScene("ajoutSecretaire");
     }
 
     @FXML
-    void crudgestionnaire(ActionEvent event) {
-        HelloApplication.changeScene("crudGestionnaire");
+    void Ajoutgestionnaire(ActionEvent event) {
+        HelloApplication.changeScene("ajoutGestionnaire");
     }
 
     @FXML
     void retourAdmin(ActionEvent event) {
-
+        HelloApplication.changeScene("Accueil");
     }
 }

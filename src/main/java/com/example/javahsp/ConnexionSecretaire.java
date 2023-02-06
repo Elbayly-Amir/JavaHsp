4ba7a77ab02dca8b59e71bd0a76c484d594de371
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-02-06 13:24:28 +0100

    connexion Secretaire

--- src/main/java/com/example/javahsp/ConnexionSecretaire.java
+++ src/main/java/com/example/javahsp/ConnexionSecretaire.java
@@ -3,6 +3,10 @@ package com.example.javahsp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import modele.Medecin;
+import modele.Secretaire;
+
+import java.sql.SQLException;
 
 public class ConnexionSecretaire {
 
@@ -13,8 +17,17 @@ public class ConnexionSecretaire {
     private TextField mdp;
 
     @FXML
-    void connexion(ActionEvent event) {
+    void connexion(ActionEvent event) throws SQLException {
 
+        Secretaire s = new Secretaire(email.getText(), mdp.getText());
+        Secretaire st = s.connexion();
+        if(s == null){
+            System.out.println("compte introuvable");
+        }else{
+            System.out.println("user connecté!");
+        }
+        System.out.println("connexion");
+        HelloApplication.changeScene("espaceSecretaire");
     }
 
     @FXML

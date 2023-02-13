31d0edc297e76dff0a11526603fb897d80748f2b
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-02-13 13:33:40 +0100

    redirection

--- src/main/java/com/example/javahsp/EspaceSecretaire.java
+++ src/main/java/com/example/javahsp/EspaceSecretaire.java
@@ -1,4 +1,23 @@
 package com.example.javahsp;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class EspaceSecretaire {
+
+
+    @FXML
+    void ajoutDossierPatient(ActionEvent event) {
+        HelloApplication.changeScene("ajoutDossier");
+    }
+
+    @FXML
+    void ajoutFichePatient(ActionEvent event) {
+        HelloApplication.changeScene("ajoutFichePatient");
+    }
+
+    @FXML
+    void decoSec(ActionEvent event) {
+        HelloApplication.changeScene("Accueil");
+    }
 }

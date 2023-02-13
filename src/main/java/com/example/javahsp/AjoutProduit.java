b66128a84a96f0908419bc2a4a048be5d48e3ce5
ryad <r.tedjani@lprs.fr>
2023-02-13 22:15:50 +0100

    Produit + ajout Produit

---
+++ src/main/java/com/example/javahsp/AjoutProduit.java
@@ -0,0 +1,38 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import modele.FichePatient;
+
+import java.sql.SQLException;
+
+import static java.lang.Integer.parseInt;
+
+public class AjoutProduit {
+
+
+    @FXML
+    private TextField DescriptionProduit;
+
+    @FXML
+    private TextField LibelleProduit;
+
+    @FXML
+    private TextField NivDangerProduit;
+
+    @FXML
+    void AjoutProduit(ActionEvent event) throws SQLException {
+        Produit ajout = new Produit(DescriptionProduit.getText(), LibelleProduit.getText(),parseInt(NivDangerProduit.getText()));
+        ajout.ajoutProduit();
+        System.out.println("Fiche Patient ajouté !");
+        HelloApplication.changeScene("espaceSecretaire");
+
+    }
+
+    @FXML
+    void retourProduit(ActionEvent event) {
+        HelloApplication.changeScene("Produit");
+
+    }
+}

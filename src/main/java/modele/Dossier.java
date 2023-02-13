4f3aeb7aa86afd8fcd36cbe7719b1f9744035189
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-02-13 13:31:42 +0100

    méthode ajout dossier de patient

--- src/main/java/modele/Dossier.java
+++ src/main/java/modele/Dossier.java
@@ -1,26 +1,51 @@
 package modele;
 
+import BDD.BDD;
+import javafx.util.StringConverter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public class Dossier {
 
     private int id_dossier;
-    private int dateDossier;
-    private int heure;
+
     private String descriprion;
     private String nivGravite;
     private int ref_fichepatient;
 
 
 
+    public Dossier(String text, String text1, int ref_fichepatient) {
+        this.descriprion=text;
+        this.nivGravite=text1;
+        this.ref_fichepatient= ref_fichepatient;
+    }
 
 
 
 
 
 
+    public void ajoutDossierPatient()  throws SQLException {
+        BDD mabdd = new BDD();
+        PreparedStatement maRequete = mabdd.getBDD().prepareStatement("INSERT INTO dossier (description,nivGravite,ref_fichepatient) VALUES (?,?,?)");
+
+        maRequete.setString(1, descriprion);
+        maRequete.setString(2, nivGravite);
+        maRequete.setInt(3, ref_fichepatient);
+        int mesResultats = maRequete.executeUpdate();
+    }
 
 
 
-
+    public Dossier(int id_dossier, String descriprion, String nivGravite, int ref_fichepatient) {
+        this.id_dossier = id_dossier;
+        this.descriprion = descriprion;
+        this.nivGravite = nivGravite;
+        this.ref_fichepatient = ref_fichepatient;
+    }
 
     public int getId_dossier() {
         return id_dossier;
@@ -30,21 +55,9 @@ public class Dossier {
         this.id_dossier = id_dossier;
     }
 
-    public int getDateDossier() {
-        return dateDossier;
-    }
 
-    public void setDateDossier(int dateDossier) {
-        this.dateDossier = dateDossier;
-    }
 
-    public int getHeure() {
-        return heure;
-    }
 
-    public void setHeure(int heure) {
-        this.heure = heure;
-    }
 
     public String getDescriprion() {
         return descriprion;

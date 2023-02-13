9c1fbd942d2e1eaf4e26ca999d0b67ceeeb24ecf
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-02-13 13:31:09 +0100

    ajout fiche patient

---
+++ src/main/java/com/example/javahsp/AjoutFichePatient.java
@@ -0,0 +1,50 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import modele.FichePatient;
+import modele.Gestionnaire;
+
+import java.sql.SQLException;
+
+import static java.lang.Integer.parseInt;
+
+public class AjoutFichePatient {
+
+    @FXML
+    private TextField cpPatient;
+
+    @FXML
+    private TextField emailPatient;
+
+    @FXML
+    private TextField nomPatient;
+
+    @FXML
+    private TextField numSecuPatient;
+
+    @FXML
+    private TextField prenomPatient;
+
+    @FXML
+    private TextField ruePatient;
+
+    @FXML
+    private TextField villePatient;
+
+    @FXML
+    void retourFichePatient(ActionEvent event) {
+        HelloApplication.changeScene("espaceSecretaire");
+    }
+    @FXML
+    void ajoutFichePatient(ActionEvent event) throws SQLException {
+        System.out.println(nomPatient.getText());
+        FichePatient ajout = new FichePatient(nomPatient.getText(), prenomPatient.getText(),parseInt(numSecuPatient.getText()), ruePatient.getText(), parseInt(cpPatient.getText()), villePatient.getText());
+        ajout.ajoutFichePatient();
+        System.out.println("Fiche Patient ajouté !");
+        HelloApplication.changeScene("espaceSecretaire");
+
+    }
+
+}

aff913335119d21426c065766acea8ac9ba411ee
amir <a.elbayly@lprs.fr>
2023-02-14 21:05:28 +0100

    méthode supprimer

--- src/main/java/modele/Chambre.java
+++ src/main/java/modele/Chambre.java
@@ -1,34 +1,23 @@
 package modele;
 
+import BDD.BDD;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Chambre {
 
     private int id_chambre;
     private String Occupation;
 
 
+    public void deleteChambre() throws SQLException {
+        BDD mabdd = new BDD();
+        PreparedStatement maRequete = mabdd.getBDD().prepareStatement("DELETE FROM chambre where id_chambre =?");
+        maRequete.setInt(1, id_chambre );
+        maRequete.executeUpdate();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
     public int getId_chambre() {

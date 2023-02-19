a7ef0871c82ebf531c83c599241fe971521f2229
amir <a.elbayly@lprs.fr>
2023-02-19 22:22:49 +0100

    formulaire ajout hospitalisation

---
+++ src/main/java/com/example/javahsp/AjoutHospitalisation.java
@@ -0,0 +1,25 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AjoutHospitalisation {
+
+    @FXML
+    private TextField maladie;
+
+    @FXML
+    private TextField numChambre;
+
+    @FXML
+    void ajoutHospitalisation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retour(ActionEvent event) {
+
+    }
+
+}

0a7e599b2d08cc24705a9df2869bcc5d35f874b7
amir <a.elbayly@lprs.fr>
2023-02-21 20:05:30 +0100

    formulaire ajout Hospitalisation

--- src/main/java/com/example/javahsp/AjoutHospitalisation.java
+++ src/main/java/com/example/javahsp/AjoutHospitalisation.java
@@ -3,6 +3,12 @@ package com.example.javahsp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import modele.Gestionnaire;
+import modele.Hospitalisation;
+
+import java.sql.SQLException;
+
+import static java.lang.Integer.parseInt;
 
 public class AjoutHospitalisation {
 
@@ -13,13 +19,17 @@ public class AjoutHospitalisation {
     private TextField numChambre;
 
     @FXML
-    void ajoutHospitalisation(ActionEvent event) {
-
+    void ajoutHospitalisation(ActionEvent event) throws SQLException {
+        Hospitalisation ajout = new Hospitalisation(maladie.getText(), parseInt(numChambre.getText()));
+        ajout.ajoutHospitalisation();
+        System.out.println("Hospitalisation ajouté !");
+        HelloApplication.changeScene("ajoutHospitalisation");
     }
 
     @FXML
-    void retour(ActionEvent event) {
-
+    void retour(ActionEvent event)  {
+      HelloApplication.changeScene("espaceMedecin");
     }
+
 
 }

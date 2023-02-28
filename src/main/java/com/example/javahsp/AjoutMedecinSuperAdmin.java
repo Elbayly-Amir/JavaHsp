7a6a3bfe1e77795d20ae16b653b4b3c2c3ef6ed2
amir <a.elbayly@lprs.fr>
2023-03-01 00:21:09 +0100

    ajout de medecin pour le super admin

---
+++ src/main/java/com/example/javahsp/AjoutMedecinSuperAdmin.java
@@ -0,0 +1,31 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AjoutMedecinSuperAdmin {
+
+    @FXML
+    private TextField emailMedecin;
+
+    @FXML
+    private PasswordField mdpMedecin;
+
+    @FXML
+    private TextField nomMedecin;
+
+    @FXML
+    private TextField prenomMedecin;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    void ajoutMedecin(ActionEvent event) {
+
+    }
+
+}

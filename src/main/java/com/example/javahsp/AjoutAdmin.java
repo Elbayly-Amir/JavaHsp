49171d41457ed9d24b96b1d11c796c700cabb3c1
amir <a.elbayly@lprs.fr>
2023-03-01 00:18:59 +0100

    ajout d'un admin

---
+++ src/main/java/com/example/javahsp/AjoutAdmin.java
@@ -0,0 +1,26 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AjoutAdmin {
+
+    @FXML
+    private TextField emailAdmin;
+
+    @FXML
+    private PasswordField mdpAdmin;
+
+    @FXML
+    void ajoutAdmin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retour(ActionEvent event) {
+
+    }
+
+}

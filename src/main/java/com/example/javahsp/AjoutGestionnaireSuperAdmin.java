80caac8aa54d47ede764576966f2aa4390d7d279
amir <a.elbayly@lprs.fr>
2023-03-01 00:20:46 +0100

    ajout de gestionnaire pour le super admin

---
+++ src/main/java/com/example/javahsp/AjoutGestionnaireSuperAdmin.java
@@ -0,0 +1,27 @@
+package com.example.javahsp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AjoutGestionnaireSuperAdmin {
+
+    @FXML
+    private TextField emailGestionnaire;
+
+    @FXML
+    private PasswordField mdpGestionnaire;
+
+    @FXML
+    private TextField nomGestionnaire;
+
+    @FXML
+    private TextField prenomGestionnaire;
+
+    @FXML
+    void ajoutGestionnaire(ActionEvent event) {
+
+    }
+
+}

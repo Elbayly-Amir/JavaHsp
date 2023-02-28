d93ce1cc0fb277e7ff1f8b37a08ebc720aa531dd
amir <a.elbayly@lprs.fr>
2023-03-01 00:21:24 +0100

    ajout de secretaire pour le super admin

---
+++ src/main/java/com/example/javahsp/AjoutSecretaireSuperAdmin.java
@@ -0,0 +1,28 @@
+package com.example.javahsp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AjoutSecretaireSuperAdmin {
+
+    @FXML
+    private Button ajoutSecretaire;
+
+    @FXML
+    private TextField emailSecretaire;
+
+    @FXML
+    private PasswordField mdpSecretaire;
+
+    @FXML
+    private TextField nomSecretaire;
+
+    @FXML
+    private TextField preSecr;
+
+    @FXML
+    private Button retour;
+
+}

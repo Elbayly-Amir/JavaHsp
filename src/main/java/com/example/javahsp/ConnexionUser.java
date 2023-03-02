6731e2105ad0cba80db3c2039364aef31a715c07
ELBAYLY_Am <a.elbayly@lprs.fr>
2023-03-02 12:05:24 +0100

    connexion changé + gestion d'erreur

--- src/main/java/com/example/javahsp/ConnexionUser.java
+++ src/main/java/com/example/javahsp/ConnexionUser.java
@@ -2,6 +2,7 @@ package com.example.javahsp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import modele.User;
@@ -16,18 +17,24 @@ public class ConnexionUser {
     @FXML
     private PasswordField mdp;
 
+
     @FXML
     void connexion(ActionEvent event) throws SQLException {
         User user = new User(email.getText(),mdp.getText());
         user.connexionUser();
+        }
 
+
+    @FXML
+    void mdpOublie(ActionEvent event) {
+        HelloApplication.changeScene("");
     }
 
 
 
     @FXML
     void retour(ActionEvent event) {
-
+        HelloApplication.changeScene("Accueil");
     }
 
 }

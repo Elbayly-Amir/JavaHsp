4aec292cf92fc4f187bba64cf57c09de94f899cf
amir-elbayly <a.elbayly@lprs.fr>
2023-03-17 21:10:56 +0100

    correction ajout de fiche sortit

--- src/main/java/com/example/javahsp/AjoutFicheSortit.java
+++ src/main/java/com/example/javahsp/AjoutFicheSortit.java
@@ -2,6 +2,7 @@ package com.example.javahsp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import modele.FichePatient;
 import modele.FicheSortit;
@@ -23,10 +24,29 @@ public class AjoutFicheSortit {
 
     @FXML
     void ajoutFicheSortit(ActionEvent event) throws SQLException {
-       FicheSortit ajout = new FicheSortit(raisonDemande.getText(),nomProduit.getText(),parseInt(QteProduit.getText()));
-        ajout.ajoutFicheSortit();
-        System.out.println("Demande de produit effectué !");
-        HelloApplication.changeScene("ajoutFicheSortit");
+
+       FicheSortit ajout = new FicheSortit(raisonDemande.getText(),nomProduit.getText(), Integer.parseInt(QteProduit.getText()));
+
+       if(raisonDemande.getText().isEmpty() || nomProduit.getText().isEmpty()){
+           Alert alert = new Alert(Alert.AlertType.WARNING);
+           alert.setTitle("Champ vide");
+           alert.setHeaderText(null);
+           alert.setContentText("Veuillez saisir des informations !!");
+           alert.showAndWait();
+       }else if(Integer.parseInt(QteProduit.getText()) <= 0){
+           Alert alert = new Alert(Alert.AlertType.WARNING);
+           alert.setTitle("Champ vide");
+           alert.setHeaderText(null);
+           alert.setContentText("Veuillez une quantité valide !!");
+           alert.showAndWait();
+       }else{
+            ajout.ajoutFicheSortit();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Ajout d'utilisateur");
+            alert.setHeaderText(null);
+            alert.setContentText("L'utilisateur a été ajouté avec succès !");
+            alert.showAndWait();
+        }
     }
 
     @FXML

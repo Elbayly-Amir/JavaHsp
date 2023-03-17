94f6d4657fac66573fd713b44f855d7498f62b47
amir-elbayly <a.elbayly@lprs.fr>
2023-03-17 21:11:48 +0100

    ajout d'hospitalisation complet

--- src/main/java/com/example/javahsp/AjoutHospitalisation.java
+++ src/main/java/com/example/javahsp/AjoutHospitalisation.java
@@ -2,7 +2,11 @@ package com.example.javahsp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
+import modele.Chambre;
 import modele.Hospitalisation;
 
 import java.sql.SQLException;
@@ -15,19 +19,37 @@ public class AjoutHospitalisation {
     private TextField maladie;
 
     @FXML
-    private TextField numChambre;
+    private SplitMenuButton liste;
 
     @FXML
     void ajoutHospitalisation(ActionEvent event) throws SQLException {
-        Hospitalisation ajout = new Hospitalisation(maladie.getText(), parseInt(numChambre.getText()));
-        ajout.ajoutHospitalisation();
-        System.out.println("Hospitalisation ajouté !");
-        HelloApplication.changeScene("ajoutHospitalisation");
+        Chambre chambre = new Chambre();
+        int idChambre = chambre.getIdChambre(liste.getText());
+        System.out.println("Nom de la chambre sélectionnée : " + liste.getText());
+        System.out.println("ID de la chambre correspondante : " + idChambre);
+        Hospitalisation ajout = new Hospitalisation(maladie.getText(), idChambre);
+        ajout.ajoutHospitalisation(idChambre);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Ajout de l'hospitalisation");
+        alert.setHeaderText(null);
+        alert.setContentText("L'hospitalisation a été ajoutée avec succès !");
+        alert.showAndWait();
     }
+
 
     @FXML
     void retour(ActionEvent event)  {
       HelloApplication.changeScene("espaceMedecin");
+    }
+
+    @FXML
+    void initialize() throws SQLException {
+        Chambre ch = new Chambre();
+        for (Chambre nomChambre : ch.selectNomChambre()) {
+            MenuItem item = new MenuItem(nomChambre.getNomChambre());
+            item.setOnAction(event -> liste.setText(item.getText()));
+            liste.getItems().add(item);
+        }
     }
 
 

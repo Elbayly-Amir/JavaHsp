92ab231526d186d5f8daef26ca47d94731458809
amir <a.elbayly@lprs.fr>
2023-03-19 23:59:50 +0100

    espace secretaire opérationnel

--- src/main/java/com/example/javahsp/EspaceSecretaire.java
+++ src/main/java/com/example/javahsp/EspaceSecretaire.java
@@ -3,20 +3,25 @@ package com.example.javahsp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import modele.Dossier;
 import modele.FichePatient;
-import modele.FicheSortit;
+import modele.User;
+
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class EspaceSecretaire implements Initializable {
+
+    @FXML
+    private MenuButton ajout;
+
+    @FXML
+    private MenuButton suppSecretaire;
 
     @FXML
     private Tab dossier;
@@ -30,6 +35,7 @@ public class EspaceSecretaire implements Initializable {
     @FXML
     private Tab patient;
 
+
     @FXML
     void ajoutDossierPatient(ActionEvent event) {
         HelloApplication.changeScene("ajoutDossier");
@@ -40,23 +46,13 @@ public class EspaceSecretaire implements Initializable {
         HelloApplication.changeScene("ajoutFichePatient");
     }
 
-    @FXML
-    void decoSec(ActionEvent event) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Déconnexion");
-        alert.setHeaderText(null);
-        alert.setContentText("Vous avez été déconnecté.");
-        alert.showAndWait();
-        HelloApplication.changeScene("connexionUser");
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         String[][] colonne = {
                 {"ID", "id_fichepatient"},
                 {"Nom", "nom"},
                 {"Prenom", "prenom"},
-                {"SecuriteSociale", "securiteSociale"},
+                {"SecuriteSocial", "securiteSocial"},
                 {"Email", "email"},
                 {"Rue", "rue"},
                 {"Cp", "cp"},
@@ -68,10 +64,31 @@ public class EspaceSecretaire implements Initializable {
             myTble.setCellValueFactory(new PropertyValueFactory<FichePatient, String>(colonne[i][1]));
             fichePatient.getColumns().add(myTble);
         }
-        // afficher les secrétaires lorsque l'onglet est sélectionné
+
+        patient.setOnSelectionChanged(event -> {
+            if (patient.isSelected()) {
+                afficherFichePatient();
+            }
+        });
+
+        String[][] colonn = {
+                {"ID", "id_dossier"},
+                {"DateDossier", "dateDossier"},
+                {"Description", "description"},
+                {"NivGravite", "nivGravite"},
+                {"RefFichePatient", "ref_fichepatient"},
+
+        };
+
+        for (int i = 0; i < colonn.length; i++) {
+            TableColumn<Dossier, String> myTble = new TableColumn<>(colonn[i][0]);
+            myTble.setCellValueFactory(new PropertyValueFactory<Dossier, String>(colonn[i][1]));
+            dossierPatient.getColumns().add(myTble);
+        }
+
         dossier.setOnSelectionChanged(event -> {
             if (dossier.isSelected()) {
-                afficherFichePatient();
+                afficherDossier();
             }
         });
     }
@@ -84,5 +101,76 @@ public class EspaceSecretaire implements Initializable {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void afficherDossier() {
+        Dossier d = new Dossier();
+        try {
+            dossierPatient.getItems().addAll(d.getDossier());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void suppDossier(ActionEvent event) {
+        Dossier doss = dossierPatient.getSelectionModel().getSelectedItem();
+        if (doss != null) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation de suppression");
+            alert.setHeaderText(null);
+            alert.setContentText("Êtes-vous sûr de vouloir supprimer cet utilisateur ?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                try {
+                    doss.deleteDossierPatient(doss);
+                    dossierPatient.getItems().remove(doss);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Aucune sélection");
+            alert.setHeaderText(null);
+            alert.setContentText("Veuillez sélectionner un gestionnaire à supprimer.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    void suppFichePatient(ActionEvent event) {
+        FichePatient fiche = fichePatient.getSelectionModel().getSelectedItem();
+        if (fiche != null) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation de suppression");
+            alert.setHeaderText(null);
+            alert.setContentText("Êtes-vous sûr de vouloir supprimer cet utilisateur ?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                try {
+                    fiche.deleteFichePatient(fiche);
+                    fichePatient.getItems().remove(fiche);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Aucune sélection");
+            alert.setHeaderText(null);
+            alert.setContentText("Veuillez sélectionner un gestionnaire à supprimer.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    void decoSec(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Déconnexion");
+        alert.setHeaderText(null);
+        alert.setContentText("Vous avez été déconnecté.");
+        alert.showAndWait();
+        HelloApplication.changeScene("connexionUser");
     }
 }

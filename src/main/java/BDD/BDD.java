cf58d77e383bfac6f893ce5e96f2354f2fabc416
amir <a.elbayly@lprs.fr>
2023-03-19 23:57:55 +0100

    BDD

--- src/main/java/BDD/BDD.java
+++ src/main/java/BDD/BDD.java
@@ -4,18 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class BDD {
-    private static Connection cnx;
+public class BDD implements AutoCloseable {
+    private Connection connection;
 
-    public static Connection cnx() {
-        return cnx;
+    // constructeur pour établir la connexion à la base de données
+    public BDD() throws SQLException {
+        // initialisation de la connexion
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hspjava", "hspjava", "FK@dAYuHV9AUx89J");
     }
 
-    public Connection getBDD() throws SQLException {
-        this.cnx = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/hspjava?serverTimezone=UTC"
-                , "hspjava"
-                , "FK@dAYuHV9AUx89J");
-        return cnx;
+    // méthode pour obtenir la connexion à la base de données
+    public Connection getBDD() {
+        return connection;
     }
+
+    // implémentation de la méthode close() pour fermer la connexion à la base de données
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
 }

078c3bcf5017278712a7326e5056a62823fbf5c8
amir <a.elbayly@lprs.fr>
2023-03-26 03:57:27 +0200

    modification de hospitalisation opérationnel

--- src/main/java/com/example/javahsp/EspaceMedecin.java
+++ src/main/java/com/example/javahsp/EspaceMedecin.java
@@ -41,6 +41,9 @@ public class EspaceMedecin implements Initializable{
     @FXML
     private TableView<Hospitalisation> viewHospitalisation;
 
+    @FXML
+    private MenuButton modif;
+
     public EspaceMedecin(){}
 
     @Override
@@ -66,7 +69,6 @@ public class EspaceMedecin implements Initializable{
         });
 
         String[][] colonne= {
-                {"ID", "id_chambre"},
                 {"Nom de la chambre", "nomChambre"},
                 {"Occupation", "occupation"},
 
@@ -85,7 +87,6 @@ public class EspaceMedecin implements Initializable{
         });
 
         String[][] colonn = {
-                {"ID", "id_dossier"},
                 {"Date Dossier", "dateDossier"},
                 {"Description de la maladie", "description"},
                 {"Gravité", "nivGravite"},
@@ -109,6 +110,7 @@ public class EspaceMedecin implements Initializable{
         Hospitalisation h = new Hospitalisation();
 
         try {
+            viewHospitalisation.getItems().clear();
             viewHospitalisation.getItems().addAll(h.getHospitalisation());
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -119,6 +121,7 @@ public class EspaceMedecin implements Initializable{
         Chambre c = new Chambre();
 
         try {
+            viewChambre.getItems().clear();
             viewChambre.getItems().addAll(c.getChambre());
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -129,6 +132,7 @@ public class EspaceMedecin implements Initializable{
         Dossier d = new Dossier();
 
         try {
+            viewDossier.getItems().clear();
             viewDossier.getItems().addAll(d.getDossier());
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -152,7 +156,7 @@ public class EspaceMedecin implements Initializable{
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Confirmation de suppression");
             alert.setHeaderText(null);
-            alert.setContentText("Êtes-vous sûr de vouloir supprimer cet utilisateur ?");
+            alert.setContentText("Êtes-vous sûr de vouloir supprimer cet hospitalisation ?");
             Optional<ButtonType> result = alert.showAndWait();
             if (result.isPresent() && result.get() == ButtonType.OK) {
                 try {
@@ -168,6 +172,36 @@ public class EspaceMedecin implements Initializable{
             alert.setHeaderText(null);
             alert.setContentText("Veuillez sélectionner une hospitalisation à supprimer.");
             alert.showAndWait();
+        }
+    }
+
+
+    @FXML
+    void modifHospitalisation(ActionEvent event) throws SQLException {
+        Hospitalisation hospitalisation = null;
+        TableView<Hospitalisation> tableView = null;
+        if (tabHospitalisation.isSelected()) {
+            tableView = viewHospitalisation;
+        }
+        if (tableView != null) {
+            hospitalisation = tableView.getSelectionModel().getSelectedItem();
+        }
+
+        // Vérifier si un utilisateur a été sélectionné
+        if (hospitalisation == null) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Sélectionner un utilisateur");
+            alert.setHeaderText(null);
+            alert.setContentText("Veuillez sélectionner un utilisateur à modifier.");
+            alert.showAndWait();
+            return;
+        } else {
+            // Récupérer l'ID de l'utilisateur sélectionné
+            int hospitalisationId = hospitalisation.getId_hospitalisation();
+
+            // Passer l'ID de l'utilisateur sélectionné à votre formulaire de modification
+            UpdateHospitalisation updateHospitalisation = new UpdateHospitalisation(hospitalisationId);
+            HelloApplication.changeScene("updateHospitalisation", new UpdateHospitalisation(hospitalisationId));
         }
     }
 

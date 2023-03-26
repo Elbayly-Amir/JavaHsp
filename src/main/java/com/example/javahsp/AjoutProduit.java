ec64fead8e1fa20c3db21f6aa9103685bf03950f
amir <a.elbayly@lprs.fr>
2023-03-26 03:45:49 +0200

    ajout de produit opérationel

--- src/main/java/com/example/javahsp/AjoutProduit.java
+++ src/main/java/com/example/javahsp/AjoutProduit.java
@@ -2,12 +2,15 @@ package com.example.javahsp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
-import modele.FichePatient;
+import modele.Produit;
 
 import java.sql.SQLException;
 
-import static java.lang.Integer.parseInt;
+
 
 public class AjoutProduit {
 
@@ -19,20 +22,44 @@ public class AjoutProduit {
     private TextField LibelleProduit;
 
     @FXML
-    private TextField NivDangerProduit;
+    private MenuItem faible;
+
+    @FXML
+    private MenuItem grave;
+
+    @FXML
+    private SplitMenuButton nivDanger;
+
+    @FXML
+    private MenuItem moyen;
+
 
     @FXML
     void AjoutProduit(ActionEvent event) throws SQLException {
-        Produit ajout = new Produit(DescriptionProduit.getText(), LibelleProduit.getText(),parseInt(NivDangerProduit.getText()));
-        ajout.ajoutProduit();
+        Produit ajout = new Produit(DescriptionProduit.getText(), LibelleProduit.getText(), nivDanger.getText());
+        ajout.AjoutProduit();
         System.out.println("Produit ajouté !");
         HelloApplication.changeScene("ajoutProduit");
-
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Ajout du produit");
+        alert.setHeaderText(null);
+        alert.setContentText("Le produit a été ajoutée avec succès !");
+        alert.showAndWait();
     }
+
+
+
+    @FXML
+    void initialize() throws SQLException {
+        faible.setOnAction(e -> nivDanger.setText("faible"));
+        moyen.setOnAction(e -> nivDanger.setText("moyen"));
+        grave.setOnAction(e -> nivDanger.setText("grave"));
+
+        }
 
     @FXML
     void retourProduit(ActionEvent event) {
-        HelloApplication.changeScene("Produit");
-
+        HelloApplication.changeScene("espaceGestionnaire");
     }
+
 }

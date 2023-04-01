46db90a0a2d4b4a7fdfaf6b5b7bb6784a03a4e4a
amir <a.elbayly@lprs.fr>
2023-04-01 13:53:31 +0200

    ajout de suvie Hospitalisation

--- src/main/java/com/example/javahsp/AjoutHospitalisation.java
+++ src/main/java/com/example/javahsp/AjoutHospitalisation.java
@@ -8,6 +8,8 @@ import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
 import modele.Chambre;
 import modele.Hospitalisation;
+import modele.SuivieHospitalisation;
+import modele.User;
 
 import java.sql.SQLException;
 
@@ -21,18 +23,26 @@ public class AjoutHospitalisation {
     @FXML
     private SplitMenuButton liste;
 
+    private int id_user;
+
+    public AjoutHospitalisation(int id_user) {
+        this.id_user = id_user;
+    }
+
     @FXML
     void ajoutHospitalisation(ActionEvent event) throws SQLException {
+        User user = new User(id_user);
         Chambre chambre = new Chambre();
         int idChambre = chambre.getIdChambre(liste.getText());
         System.out.println("Nom de la chambre sélectionnée : " + liste.getText());
         System.out.println("ID de la chambre correspondante : " + idChambre);
         Hospitalisation ajout = new Hospitalisation(maladie.getText(), idChambre);
-        ajout.ajoutHospitalisation(idChambre);
+        int id_hospitalisation =  ajout.ajoutHospitalisation(idChambre,id_user);
 
         // Mettre à jour l'occupation de la chambre en "Occupe"
         chambre.updateOccupation("Occupe", idChambre);
-
+        SuivieHospitalisation sh = new SuivieHospitalisation();
+        sh.AjoutSuivieHospitalisation(id_user,id_hospitalisation);
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Ajout de l'hospitalisation");
         alert.setHeaderText(null);
@@ -44,7 +54,7 @@ public class AjoutHospitalisation {
 
     @FXML
     void retour(ActionEvent event)  {
-      HelloApplication.changeScene("espaceMedecin");
+      HelloApplication.changeScene("espaceMedecin",new EspaceMedecin());
     }
 
     @FXML

8260964d256d8d3317d66207d8e3c0a596841454
amir <a.elbayly@lprs.fr>
2023-04-01 13:54:03 +0200

    ajout de suvie Fiche Sortit

--- src/main/java/com/example/javahsp/AjoutFicheSortit.java
+++ src/main/java/com/example/javahsp/AjoutFicheSortit.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import modele.FichePatient;
 import modele.FicheSortit;
+import modele.SuivieFicheSortit;
+import modele.User;
 
 import java.sql.SQLException;
 
@@ -22,11 +24,17 @@ public class AjoutFicheSortit {
     @FXML
     private TextField raisonDemande;
 
+    private int id_user;
+
+    public AjoutFicheSortit(int id_user) {
+        this.id_user= id_user;
+    }
+
     @FXML
     void ajoutFicheSortit(ActionEvent event) throws SQLException {
-
+        User user = new User(id_user);
        FicheSortit ajout = new FicheSortit(raisonDemande.getText(),nomProduit.getText(), Integer.parseInt(QteProduit.getText()));
-
+        SuivieFicheSortit sf = new SuivieFicheSortit();
        if(raisonDemande.getText().isEmpty() || nomProduit.getText().isEmpty()){
            Alert alert = new Alert(Alert.AlertType.WARNING);
            alert.setTitle("Champ vide");
@@ -40,7 +48,8 @@ public class AjoutFicheSortit {
            alert.setContentText("Veuillez une quantité valide !!");
            alert.showAndWait();
        }else{
-            ajout.ajoutFicheSortit();
+            int id_fichesortit = ajout.ajoutFicheSortit(id_user);
+            sf.AjoutSuivieFicheSortit(id_user, id_fichesortit);
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Ajout d'utilisateur");
             alert.setHeaderText(null);
@@ -51,6 +60,6 @@ public class AjoutFicheSortit {
 
     @FXML
     void retour(ActionEvent event) {
-        HelloApplication.changeScene("espaceMedecin");
+        HelloApplication.changeScene("espaceMedecin",new EspaceMedecin());
     }
 }
